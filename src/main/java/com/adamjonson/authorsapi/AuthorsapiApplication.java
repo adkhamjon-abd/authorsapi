69fd62767c7396086940ae403dd72bb87abbe4fc
Hp <adkhamjon.abd@gmail.com>
2024-06-01 14:37:01 +0200

    create the project and add an author entity

---
+++ src/main/java/com/adamjonson/authorsapi/AuthorsapiApplication.java
@@ -0,0 +1,13 @@
+package com.adamjonson.authorsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamjonson/authorsapi/AuthorsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamjonson.authorsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
