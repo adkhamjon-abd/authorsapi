2d0ffd8a6ee0ccf1c7a95d6fde312e6e9d3ae579
Hp <adkhamjon.abd@gmail.com>
2024-06-02 13:34:45 +0200

    Complete the logic of the method in service class to find author by name

--- src/main/java/com/adamjonson/authorsapi/model/Author.java
+++ src/main/java/com/adamjonson/authorsapi/model/Author.java
@@ -14,5 +14,6 @@ public class Author {
     private int id;
 
     private String name;
+    private String openLibraryId;
 
 }
--- src/main/java/com/adamjonson/authorsapi/service/AuthorService.java
+++ src/main/java/com/adamjonson/authorsapi/service/AuthorService.java
@@ -5,24 +5,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.adamjonson.authorsapi.api.OpenLibraryApi;
 import com.adamjonson.authorsapi.repo.AuthorRepository;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AuthorService {
 
     @Autowired
     AuthorRepository authorRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String OPEN_LIBRARY_API_SEARCH_URL = "https://openlibrary.org/search/authors.json?q=";
+    private static final String OPEN_LIBRARY_API_AUTHOR_URL = "https://openlibrary.org/authors/";
+
     public Author findAuthorByName(String name) {
         Author author = authorRepository.findByName(name);
         if (author != null) {
             return author;
         } else {
-            Author externalAuthor = OpenLibraryApi.fetchAuthorByName(name);
-            if (externalAuthor != null) {
-                authorRepository.save(externalAuthor);
-                return externalAuthor;
-            } else {
-                return null;
+            // Call external API
+            String url = OPEN_LIBRARY_API_SEARCH_URL + name;
+            OpenLibraryApi response = restTemplate.getForObject(url, OpenLibraryApi.class);
+            if (response != null && !response.getDocs().isEmpty()) {
+                OpenLibraryApi.Doc doc = response.getDocs().get(0);
+
+                // Fetch detailed author data using the key
+                String authorDetailsUrl = OPEN_LIBRARY_API_AUTHOR_URL + doc.getKey() + ".json";
+                OpenLibraryApiAuthorDetails authorDetails = restTemplate.getForObject(authorDetailsUrl, ExternalApiAuthorDetails.class);
+
+                if (authorDetails != null) {
+                    Author fetchedAuthor = new Author();
+                    fetchedAuthor.setName(authorDetails.getName());
+                    fetchedAuthor.setOpenLibraryId(authorDetails.getKey());
+                    authorRepository.save(fetchedAuthor);
+                    return fetchedAuthor;
+                }
             }
+            return null;
         }
 
     }
