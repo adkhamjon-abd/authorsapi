7a0b07fddd33ffdb1128d484f5aba3d32aa22254
Hp <adkhamjon.abd@gmail.com>
2024-06-02 07:44:28 +0200

    Switch to Postgres and fix type in application.properties

--- src/main/java/com/adamjonson/authorsapi/repo/AuthorRepository.java
+++ src/main/java/com/adamjonson/authorsapi/repo/AuthorRepository.java
@@ -1,2 +1,12 @@
-package com.adamjonson.authorsapi;public interface AuthorRepository {
+package com.adamjonson.authorsapi.repo;
+
+
+import com.adamjonson.authorsapi.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    Author findByName(String name);
 }
--- src/main/java/com/adamjonson/authorsapi/service/AuthorService.java
+++ src/main/java/com/adamjonson/authorsapi/service/AuthorService.java
@@ -1,12 +1,16 @@
 package com.adamjonson.authorsapi.service;
 
 import com.adamjonson.authorsapi.model.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.adamjonson.authorsapi.api.OpenLibraryApi;
+import com.adamjonson.authorsapi.repo.AuthorRepository;
 
 @Service
 public class AuthorService {
 
+    @Autowired
+    AuthorRepository authorRepository;
     public Author findAuthorByName(String name) {
         Author author = authorRepository.findByName(name);
         if (author != null) {
