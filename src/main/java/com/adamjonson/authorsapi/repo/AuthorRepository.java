c4ad3967cf619c1de9ff7f19e06a4644d4972c8b
Hp <adkhamjon.abd@gmail.com>
2024-06-02 20:14:10 +0200

    Add a new method to AuthorRepository class to fetch authors by id in OpenLibrary

--- src/main/java/com/adamjonson/authorsapi/repo/AuthorRepository.java
+++ src/main/java/com/adamjonson/authorsapi/repo/AuthorRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
     Author findByName(String name);
+
+    Author findByOpenLibraryId(String authorId);
 }
